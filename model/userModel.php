201378b2060490ab531173e2662ff5099a61acf7
Thomas Guegan <tguegan@etud.iut-orsay.fr>
2021-11-30 11:49:26 +0100

    Added user related query

--- model/userModel.php
+++ model/userModel.php
@@ -1,7 +1,40 @@
 <?php
 
+require_once __DIR__ . "/conf/Connexion.php";
+Connexion::connect();
+
 class User {
 
-}
+    private $name;
+    private $balance;
 
-?>
+    public function __construct($name, $balance) {
+        $this->name = $name;
+        $this->balance = balance;
+    }
+
+    public static function getUser($id) {
+        $query = "select * from user where id_user = :id";
+        $p_query = Connexion::getPdo()->prepare($query);
+        $result = [];
+        try {
+            $result = $p_query->execute(array("id" => $id))->fetch();
+        } catch (PDOException $e) {
+            $result["error"] = $e->getMessage();
+        }
+        return $result;
+    }
+
+    public static function getAllUser() {
+        $query = "select * from user";
+        $p_query = Connexion::getPdo()->prepare($query);
+        $result = [];
+        try {
+            $result = $p_query->execute()->fetchAll();
+        } catch (PDOException $e) {
+            $result["error"] = $e->getMessage();
+        }
+        return $result;
+    }
+
+}
