1be794e1fc27a3032c562e29c641ade5267ccaa1
Florent Weltmann <florent.weltmann@universite-paris-saclay.fr>
2021-12-24 19:12:03 +0100

    Update User Model

--- model/userModel.php
+++ model/userModel.php
@@ -1,36 +1,54 @@
 <?php
 
-require_once __DIR__ . "/conf/Connexion.php";
+require_once "conf/Connexion.php";
 Connexion::connect();
 
 class User {
-
     private $name;
+    private $password;
     private $balance;
 
-    public function __construct($name, $balance) {
-        $this->name = $name;
-        $this->balance = $balance;
+    // Getter d'un utilisateur
+	public function getName() {return $this->name;}
+	public function getPassword() {return $this->password;}
+    public function getBalance() {return $this->balance;}
+
+    // Setter d'un service
+	public function setName($name) {$this->name = $name;}
+    public function setPassword($password) {$this->password = $password;}
+    public function setBalance($balance) {$this->balance = $balance;}
+
+    // Constructeur d'un utilisateur
+    public function __construct($name = NULL, $password = NULL, $balance = NULL) {
+        if(!is_null($name) && (!is_null($password) && (!is_null($balance)))) {
+            $this->name = $name;
+            $this->password = $password;
+            $this->balance = $balance;
+        }
     }
 
     public static function getUser($id) {
-        $query = "select * from user where id_user = :id";
+        $query = "SELECT id, name, password, balance FROM users WHERE id = :id";
         $p_query = Connexion::getPdo()->prepare($query);
         $result = [];
         try {
-            $result = $p_query->execute(array("id" => $id))->fetch();
+            $p_query->setFetchMode(PDO::FETCH_CLASS, 'User');
+            $p_query->execute(array("id" => $id));
+            $result = $p_query->fetch();
         } catch (PDOException $e) {
             $result["error"] = $e->getMessage();
         }
         return $result;
     }
 
-    public static function getAllUser() {
-        $query = "select * from user";
+    public static function getAllUsers() {
+        $query = "SELECT id, name, password, balance from users";
         $p_query = Connexion::getPdo()->prepare($query);
         $result = [];
         try {
-            $result = $p_query->execute()->fetchAll();
+            $p_query->setFetchMode(PDO::FETCH_CLASS, 'User');
+            $p_query->execute();
+            $result = $p_query->fetchAll(); 
         } catch (PDOException $e) {
             $result["error"] = $e->getMessage();
         }
