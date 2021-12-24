dd4bc1dfc4bf919b1fca34e42b63c4dd424c52e8
Florent Weltmann <florent.weltmann@universite-paris-saclay.fr>
2021-12-24 19:21:16 +0100

    Added id for the Service Class and the User Class

--- model/serviceModel.php
+++ model/serviceModel.php
@@ -4,7 +4,7 @@ require_once "conf/Connexion.php";
 Connexion::connect();
 
 class Service {
-
+    private $id;
     private $date_start;
     private $date_end;
     private $id_type_service;
@@ -13,6 +13,7 @@ class Service {
     private $title;
 
     // Getter d'un service
+    public function getId() {return $this->id;}
 	public function getDateStart() {return $this->date_start;}
 	public function getDateEnd() {return $this->date_end;}
     public function getIdTypeService() {return $this->id_type_service;}
@@ -21,6 +22,7 @@ class Service {
     public function getTitle() {return $this->title;}
 
     // Setter d'un service
+    public function setId($id) {$this->id = $id;}
 	public function setDateStart($date_start) {$this->date_start = $date_start;}
     public function setDateEnd($date_end) {$this->date_end = $date_end;}
     public function setIdTypeService($id_type_service) {$this->id_type_service = $id_type_service;}
@@ -29,8 +31,9 @@ class Service {
     public function setTitle($title) {$this->title = $title;}
 
     // Constructeur d'un service
-    public function __construct($date_start = NULL, $date_end = NULL, $id_type_service = NULL, $price = NULL, $id_user = NULL, $title = NULL) {
-        if(!is_null($date_start) && !is_null($date_end) && !is_null($id_type_service) && !is_null($price) && !is_null($id_user) && !is_null($title)) {
+    public function __construct($id = NULL, $date_start = NULL, $date_end = NULL, $id_type_service = NULL, $price = NULL, $id_user = NULL, $title = NULL) {
+        if(!is_null($id) && !is_null($date_start) && !is_null($date_end) && !is_null($id_type_service) && !is_null($price) && !is_null($id_user) && !is_null($title)) {
+            $this->id = $id;
             $this->date_start = $date_start;
             $this->date_end = $date_end;
             $this->id_type_service = $id_type_service;
--- model/userModel.php
+++ model/userModel.php
@@ -4,23 +4,27 @@ require_once "conf/Connexion.php";
 Connexion::connect();
 
 class User {
+    private $id;
     private $name;
     private $password;
     private $balance;
 
     // Getter d'un utilisateur
+    public function getId() {return $this->id;}
 	public function getName() {return $this->name;}
 	public function getPassword() {return $this->password;}
     public function getBalance() {return $this->balance;}
 
     // Setter d'un service
+    public function setId($id) {$this->id = $id;}
 	public function setName($name) {$this->name = $name;}
     public function setPassword($password) {$this->password = $password;}
     public function setBalance($balance) {$this->balance = $balance;}
 
     // Constructeur d'un utilisateur
-    public function __construct($name = NULL, $password = NULL, $balance = NULL) {
-        if(!is_null($name) && (!is_null($password) && (!is_null($balance)))) {
+    public function __construct($id = NULL, $name = NULL, $password = NULL, $balance = NULL) {
+        if(!is_null($id) && !is_null($name) && (!is_null($password) && (!is_null($balance)))) {
+            $this->id = $id;
             $this->name = $name;
             $this->password = $password;
             $this->balance = $balance;
