bec8c782ab1e837d6778d76e3b1b78b8a24cf21f
pperez <pablo.perez1@universite-paris-saclay.fr>
2022-03-17 12:35:58 +0100

    Update css

--- view/Navbar.php
+++ view/Navbar.php
@@ -1,4 +1,4 @@
-<nav style="font-family: 'Roboto Mono', monospace; background-color: #2D529C; font-color: #FFFFFF" class="navbar navbar-expand-lg static-top mb-3">
+<nav style="font-family: 'Roboto Mono', monospace; color: #FFFFFF; padding: 20px " class="navbar narbar-dark navbar-expand-lg static-top mb-3">
     <div class="container px-4 px-lg-5">
         <a class="navbar-brand" href="#!"><img class="brand" src="assets/css/" alt="logo"></img></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
