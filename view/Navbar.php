4eeca89cb5a739cc6cb8bcf6a703ff56df5ad924
Perez Pablo <pablo.perez1@universite-paris-saclay.fr>
2022-03-29 09:50:26 +0200

    Update view/Navbar.php

--- view/Navbar.php
+++ view/Navbar.php
@@ -1,12 +1,18 @@
-<nav style="font-family: 'Roboto Mono', monospace; padding: 20px " class="navbar navbar-dark bg-dark navbar-expand-lg static-top mb-3">
-    <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#!"><img class="brand" src="assets/css/" alt="logo"></img></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item me-1"><a class="nav-link active" aria-current="page" href="#!"><i class="bi bi-plus-circle"style="margin-right:0.5rem"></i>FR</a></li>
-                <li class="nav-item me-1"><a class="nav-link active" aria-current="page" href="index.php?controller=controllerHome&action=displayProfile"><i class="bi bi-person-circle"style="margin-right:0.5rem"></i>Compte</a></li>
+    <nav style="font-family: 'Roboto Mono', monospace; padding: 20px " class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#!"><img class="brand" src="assets/css/" alt="logo"></img></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul style="font-size: 20px; margin-top:30px" class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item me-1">
+                    <a class="nav-link active " aria-current="page" href="#!"><i class="bi bi-plus-circle"style="margin-right:0.5rem"></i>FR</a>
+                </li>
+                <li class="nav-item me-1">
+                    <a class="nav-link active" aria-current="page" href="index.php?controller=controllerHome&action=displayProfile"><i class="bi bi-person-circle"style="margin-right:0.5rem"></i>Compte</a>
+                </li>
             </ul>
+          </div>
         </div>
-    </div>
-</nav>
+      </nav>
