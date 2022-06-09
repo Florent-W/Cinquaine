2f8af57084853e9b1265f3bb690b9a2c05970dd7
ajust <alexandre.just@universite-paris-saclay.fr>
2022-06-09 10:17:55 +0200

    Normalement login & register avec hash fonctionnels! signed 3nz0 @l3X

--- view/RegisterForm.php
+++ view/RegisterForm.php
@@ -1,10 +1,10 @@
 <form action="index.php?action=register&controller=controllerLogin" method="post">
     <div class="container">
         <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text" placeholder="Enter Username" name="uname" maxlength="20" required>
 
         <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password" placeholder="Enter Password" name="psw" maxlength="20" required>
 
         <label for="email"><b>Email</b></label>
         <input type="email" placeholder="Enter your mail" name="email" required>
