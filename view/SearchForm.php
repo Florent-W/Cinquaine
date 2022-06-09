5ff5875d1c5abb35b6bf1d7bd99a366c5ed4879a
pperez <pablo.perez1@universite-paris-saclay.fr>
2022-06-09 10:38:01 +0200

    update SearchForm.php

--- view/SearchForm.php
+++ view/SearchForm.php
@@ -1,23 +1,25 @@
-<form action="view/Homepage.php" method="post">
-    <div class="container">
-        <form class="form-inline" action="/recherche/" method="get">
-            <fieldset>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                <select id="oCategorie" name="oCategorie" class="form-control">
-                    <option selected="selected" value="0">Catégorie</option>
-                    <option value="1">Voyage</option>
-                    <option value="5">Jeux</option>
-                    <option value="3">Loisir</option>
-                    <option value="4">Informatique</option>
-                </select>
+<section  class="py-5">
+    <form action="view/Homepage.php" method="post">
+        <div class="container">
+            <form class="form-inline" action="/recherche/" method="get">
+                <fieldset>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                    <select id="oCategorie" name="oCategorie" class="form-control">
+                        <option selected="selected" value="0">Catégorie</option>
+                        <option value="1">Voyage</option>
+                        <option value="5">Jeux</option>
+                        <option value="3">Loisir</option>
+                        <option value="4">Informatique</option>
+                    </select>
+                    </div>
+                    <input id="oSaisie" name="oSaisie" type="text" class="form-control" aria-label="Saisie de mots clés" required="required" placeholder="Saisie de mots clés . . .">
+                    <div class="input-group-append">
+                    <button class="btn btn-secondary" type="submit" style="width:200px">Recherche</button>
+                    </div>
                 </div>
-                <input id="oSaisie" name="oSaisie" type="text" class="form-control" aria-label="Saisie de mots clés" required="required" placeholder="Saisie de mots clés . . .">
-                <div class="input-group-append">
-                <button class="btn btn-secondary" type="submit" style="width:200px">Recherche</button>
-                </div>
-            </div>
-            </fieldset>
-        </form>
-    </div>
-</form>
+                </fieldset>
+            </form>
+        </div>
+    </form>
+</section>
