052a0c939eec2b99a9651cef1f62a23c7efefda6
Guegan Thomas <thomas.guegan@universite-paris-saclay.fr>
2022-06-09 12:09:45 +0200

    Update ServicCard, requires service variable

--- view/ServiceCard.php
+++ view/ServiceCard.php
@@ -1,20 +1,37 @@
+<?php
+$imgSrc = "assets/image/" + $service->getTypeService() + ".jpg";
+?>
 <div class="card h-100 bg-light text-dark">
-    <div class="badge bg-dark position-absolute" style="top: 0.5rem; right: 0.5rem">Promo</div>
-    <img class="card-img-top" src="assets/image/voyage.jpg" style="object-fit: cover; height: 60%;"/>
+    <div class="badge bg-dark position-absolute" style="top: 0.5rem; right: 0.5rem">
+    <?php
+        $service->getIdTypeService();
+    ?>
+    </div>
+    <img class="card-img-top" 
+        src="<?php echo $imgSrc; ?>" 
+        style="object-fit: cover; height: 60%;"/>
     <div class="card-body p-4">
         <div class="text-center">
-            <h5 class="fw-bolder">Conseil Voyage</h5>
-            <div class="d-flex justify-content-center small text-warning mb-2">
-                <div class="bi-star-fill"></div>
-                <div class="bi-star-fill"></div>
-                <div class="bi-star-fill"></div>
-                <div class="bi-star-fill"></div>
-                <div class="bi-star-fill"></div>
+            <h5 class="fw-bolder">
+            <?php
+                echo $service->getTitle();
+            ?>
+            </h5>
+            <div class="d-flex justify-content-center small mb-2">
+                <span>
+                    <?php echo $service->getDateStart(); ?>
+                     - 
+                    <?php echo $service->getDateEnd(); ?>
+                </span>
             </div>
-            <span>18.00€</span>
+            <span>
+            <?php
+                echo $service->getPrice();
+            ?>
+            </span>
         </div>
     </div>
     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Ajouter au panier</a></div>
+        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Acheter</a></div>
     </div>
 </div>
