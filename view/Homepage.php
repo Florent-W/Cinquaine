7b6a2a140f27e20423f37935ec62841eedb7450f
toemah <toemah@protonmail.com>
2022-06-10 17:30:48 +0200

    Update Homepage.php

--- view/Homepage.php
+++ view/Homepage.php
@@ -1,9 +1,9 @@
 <!-- Section-->
 <div class="container px-4 px-lg-5 mt-5">
-    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 gap-2 mb-2 justify-content-center">
+    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gap-2 mb-2 justify-content-center">
         <?php
             foreach($services as $service) {
-                echo "<div style='height: 600px; width: 500px'>";
+                echo "<div class='espaceformodal' data-bs-toggle='modal' data-bs-target='#DescriptionModal' style='height: 600px; width: 500px'>";
                 require "ServiceCard.php";
                 echo "</div>";
             }
