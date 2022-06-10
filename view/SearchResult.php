267fcc73784ae3a626574ac29dec3e1dbdae7252
toemah <toemah@protonmail.com>
2022-06-10 23:24:55 +0200

    Update SearchResult.php
    Synced image with category
    Formatting

--- view/SearchResult.php
+++ view/SearchResult.php
@@ -20,17 +20,18 @@
     $page = 1;
     $itemperpage = 10;
 
-    if(isset($_GET["page"])) {
-        $page = htmlspecialchars($_GET["page"]);
+    if (isset($_GET["page"])) {
+      $page = htmlspecialchars($_GET["page"]);
     }
 
 
-    for($i = ($page-1) * $itemperpage; $i <($itemperpage * $page)-1; $i++) {
-
+    for ($i = ($page - 1) * $itemperpage; $i < ($itemperpage * $page) - 1; $i++) {
+      $serviceName = TypeService::getTypeService($filteredservices[$i]->getIdTypeService())->getName();
+      $imgSrc = "assets/image/" . $serviceName . ".jpg";
       echo "<div class='card mb-3' style='max-width: 900px;'>
                           <div class='row g-0'>
                             <div class='col-md-4'>
-                              <img src='assets/image/dessin.jpg' class='img-fluid rounded-start' alt='...'>
+                              <img src=" . $imgSrc . " class='img-fluid rounded-start' alt='...'>
                             </div>
                             <div class='col-md-8'>
                               <div class='card-body'>
@@ -43,28 +44,26 @@
                         </div>";
     }
 
-        echo "  <div aria-label='Page navigation'>
+    echo "  <div aria-label='Page navigation'>
                     <ul class='pagination'>";
 
-            if($page > 1) {
-                echo "<li class='page-item'>
-                        <a class='page-link' href='index.php?controller=controllerSearch&action=displaySearch&page=".strval($page-1)."&oSaisie=".$_GET["oSaisie"]."&oCategorie=".$_GET["oCategorie"]."'>Précédent
+    if ($page > 1) {
+      echo "<li class='page-item'>
+                        <a class='page-link' href='index.php?controller=controllerSearch&action=displaySearch&page=" . strval($page - 1) . "&oSaisie=" . $_GET["oSaisie"] . "&oCategorie=" . $_GET["oCategorie"] . "'>Précédent
                         </a>
                       </li>";
-            }
+    }
 
-        for($i = 1; $i <= intdiv(count($filteredservices),$itemperpage); $i++) {
-            echo "<li class='page-item'><a class='page-link' href='index.php?controller=controllerSearch&action=displaySearch&page=".strval($i)."&oSaisie=".$_GET["oSaisie"]."&oCategorie=".$_GET["oCategorie"]."'>".$i."</a></li>";
-        }
-        if(($page) < intdiv(count($filteredservices),$itemperpage)) {
-            echo "<li class='page-item'><a class='page-link' href='index.php?controller=controllerSearch&action=displaySearch&page=".strval($page+1)."&oSaisie=".$_GET["oSaisie"]."&oCategorie=".$_GET["oCategorie"]."'>Suivant</a></li>";
-        }
+    for ($i = 1; $i <= intdiv(count($filteredservices), $itemperpage); $i++) {
+      echo "<li class='page-item'><a class='page-link' href='index.php?controller=controllerSearch&action=displaySearch&page=" . strval($i) . "&oSaisie=" . $_GET["oSaisie"] . "&oCategorie=" . $_GET["oCategorie"] . "'>" . $i . "</a></li>";
+    }
+    if (($page) < intdiv(count($filteredservices), $itemperpage)) {
+      echo "<li class='page-item'><a class='page-link' href='index.php?controller=controllerSearch&action=displaySearch&page=" . strval($page + 1) . "&oSaisie=" . $_GET["oSaisie"] . "&oCategorie=" . $_GET["oCategorie"] . "'>Suivant</a></li>";
+    }
 
-        echo "
+    echo "
                     </ul>
                   </div>";
-
-
   } else {
     echo "<p> Aucun résultat pour cette recherche </p>";
   }
