b98345846e98f64cfd8e352842eb046c4ede9320
Perez Pablo <pablo.perez1@universite-paris-saclay.fr>
2022-06-10 13:48:20 +0200

    update ConnectionForm

--- view/ConnectionForm.php
+++ view/ConnectionForm.php
@@ -2,20 +2,20 @@
     <form action="index.php?action=login&controller=controllerLogin" method="post" class="loginform">
 
         <div class="form-group">
-            <label for="uname" ><b>Login :</b></label>
-            <input type="text" placeholder="Entrer le login" name="uname" required>
+            <label for="uname" ><b>Nom d'utilisateur:</b></label>
+            <input type="text" placeholder="Entrez votre nom d'utilisateur" name="uname" required>
         </div>
         <div class="form-group">
-            <label for="psw"><b>Mot de passe :</b></label>
+            <label for="psw"><b>Mot de passe:</b></label>
             <input type="password" placeholder="Entrer le mot de passe" name="psw" required>
         </div>
         <button class="btn" type="submit">Connexion</button>
         <div><button class="btn btnregister"><a class="btn" href="index.php?controller=controllerLogin&action=displayRegister">Inscription</a></button></div>
-        <div class="form-check">
+        <div class="form-check" style="margin-top: 10px; margin-right: 20px; ">
             <input type="checkbox" checked="checked" name="remember">
             <label>Se souvenir de moi</label>
         </div>
-        <div class="psw"><a href="#">Mot de passe oublié?</a></div>
+        <div class="psw" style="margin-top: 10px"><a href="#">Mot de passe oublié?</a></div>
         
     </form>
 </div>
