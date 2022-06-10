690918a1030383a2844d9cc0cde5710351d2a805
toemah <toemah@protonmail.com>
2022-06-10 18:40:05 +0200

    Update Homepage.php

--- view/Homepage.php
+++ view/Homepage.php
@@ -3,7 +3,7 @@
     <div class="row row-cols-2 row-cols-lg-3 row-cols-xl-4 gap-2 mb-2 justify-content-center">
         <?php
         foreach ($services as $service) {
-            echo "<div class='col' style='height: 600px; width: 500px'>";
+            echo "<div class='col' style='width: 500px'>";
             require "ServiceCard.php";
             echo "</div>";
         }
