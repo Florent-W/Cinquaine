823edab7ac3a74c8bd2eccdcefb1cdf6f4e8e6e9
hydroyoko <91729066+hydroyoko@users.noreply.github.com>
2022-06-10 12:55:09 +0200

    Extended Seeder + DB / Table script fixed
    Now 55 services (including 30 from type jardinage from the user Robert)

--- controller/controllerSeeder.php
+++ controller/controllerSeeder.php
@@ -9,7 +9,8 @@ class controllerSeeder {
             ["jeanbon", "cochonou", 1000, "jeanbon2000@orange.fr", "+33606060606"],
             ["pierrepaul", "jacques", 1300, "ppj91400@gmail.com", "+33707070707"],
             ["doralexplorateur", "jetaimebabouche", 1300, "doraexp@gmail.com", "+33699999999"],
-            ["calvin.yapi", "blagueurdufutur", 900, "calvin.yapi@outlook.com", "+33505050505"]
+            ["calvin.yapi", "blagueurdufutur", 900, "calvin.yapi@outlook.com", "+33505050505"],
+            ["robertjardineur", "petunias12345", 900, "robert.jardineur@club-internet.fr", "+330640060606"]
         ];
         
         foreach($userDataSets as $insertedDataSet) {
@@ -48,34 +49,66 @@ class controllerSeeder {
         $serviceEndDate = date("Y-m-d H:i:s", strtotime("+7 day"));
 
         $serviceDataSets = [
-            [1, 1, "Faire une paella"],
-            [1, 1, "Apprendre à préparer une tarte aux fruits"],
-            [1, 1, "Venez manger un curry chez moi"],
-            [2, 1, "Dessiner un chien dans un hamac"],
-            [2, 1, "Faire des traits droits"],
-            [2, 1, "Dessiner des extraterrestres"],
-            [3, 2, "Booster vos ventes sur internet"],
-            [3, 2, "Créer un site Shopify"],
-            [3, 2, "Faire du marketing digital avec Instagram"],
-            [4, 2, "Installation de logiciels"],
-            [4, 2, "Formattage de disque dur"],
-            [4, 2, "Cours d'informatique"],
-            [5, 3, "Tondre la pelouse"],
-            [5, 3, "Avoir une allée fleurie"],
-            [5, 3, "Potager dans votre jardin"],
-            [6, 3, "Booster votre perso dofus"],
-            [6, 3, "Coach League of Legends - 2H"],
-            [6, 3, "Apprendre à jouer à Tetris"],
-            [7, 4, "Faire une randonnée"],
-            [7, 4, "Le tennis pour les nuls"],
-            [7, 4, "Allons à la piscine"],
-            [8, 4, "Co-voiturage jusqu'à Bankok"],
-            [8, 4, "Comment faire du stop partout ?"],
-            [8, 4, "Tutoriel camping sauvage"]
+            [1, 1, "Faire une paella", "Expert en cuisine depuis environ 10 ans, je suis chef. Je vous propose ce service pendant lequel nous allons, ensemble, étape par étape, apprendre à préparer une paella. J'espère que cela vous plaira."],
+            [1, 1, "Apprendre à préparer une tarte aux fruits", "Expert en cuisine depuis environ 10 ans, je suis chef. Je vous propose ce service pendant lequel nous allons, ensemble, étape par étape, apprendre à préparer une tarte aux fruits (fraises, bananes, mirabelles...). J'espère que cela vous plaira."],
+            [1, 1, "Venez manger un curry chez moi", "Expert en cuisine depuis environ 10 ans, je suis chef. Je vous propose ce service pendant lequel nous allons, ensemble, étape par étape, apprendre à préparer un curry de légumes contenant du poisson ou une viande de votre choix. J'espère que cela vous plaira."],
+            [2, 1, "Dessiner un chien dans un hamac", "Diplômé d'un bac dessin, je cherche à prouver au monde qu'il est facile de dessiner. Dessinons ensemble un chien dans un hamac !"],
+            [2, 1, "Faire des traits droits" , "Diplômé d'un bac dessin, je cherche à prouver au monde qu'il est facile de dessiner. Apprenons à faire des traits droits. Pratique pour les métiers manuels ou d'architecture."],
+            [2, 1, "Dessiner des extraterrestres" , "Diplômé d'un bac dessin, je cherche à prouver au monde qu'il est facile de dessiner. Dessinons ensemble un extraterrestre dans sa soucoupe qui enlève un agriculteur et sa vache."],
+            [3, 2, "Booster vos ventes sur internet", "Vous êtes pauvre ? Arrêtez tout de suite ! Devenez riche grâce à mes cours d'e-commerce. Apprenez dès maintenant comment booster les ventes de votre site internet !"],
+            [3, 2, "Créer un site Shopify", "Vous êtes pauvre ? Arrêtez tout de suite ! Devenez riche grâce à mes cours d'e-commerce. Apprenez dès maintenant comment créer une boutique en ligne avec Shopify."],
+            [3, 2, "Faire du marketing digital avec Instagram", "Vous êtes pauvre ? Arrêtez tout de suite ! Devenez riche grâce à mes cours d'e-commerce. Apprenez dès maintenant comment marketer sur Instagram."],
+            [4, 2, "Installation de logiciels", "Ingénieur informaticien, je peux prendre la main à distance pour installer des logiciels sur votre ordinateur."],
+            [4, 2, "Formattage de disque dur", "Ingénieur informaticien, je peux prendre la main à distance pour formatter votre disque dur."],
+            [4, 2, "Cours d'informatique", "Ingénieur informaticien, je vous propose des cours d'informatique à distance."],
+            [5, 3, "Tondre la pelouse", "Je peux tondre votre pelouse."],
+            [5, 3, "Avoir une allée fleurie", "Ayant des excédents de graines et de boutures de fleurs, je peux fleurir votre allée."],
+            [5, 3, "Potager dans votre jardin", "J'ai des variétés anciennes de légumes que je souhaite partager, nous pouvons ensemble les utiliser pour vous faire un potager."],
+            [6, 3, "Booster votre perso dofus", "PGM - Je vais boost ton perso dofus."],
+            [6, 3, "Coach League of Legends - 2H", "PGM - Je vais te coach sur LoL."],
+            [6, 3, "Apprendre à jouer à Tetris", "PGM - Je vais t'apprendre à jouer à Tetris."],
+            [7, 4, "Faire une randonnée", "Profitons du monde qui nous entoure ! Partons faire une randonnée !"],
+            [7, 4, "Le tennis pour les nuls", "Profitons du monde qui nous entoure ! Apprenez à jouer au tenis."],
+            [7, 4, "Allons à la piscine", "Profitons du monde qui nous entoure ! Allons nager !"],
+            [8, 4, "Co-voiturage jusqu'à Bankok", "Partons en voyage ensemble, j'ai une voiture et nous partirons à Bankok avec."],
+            [8, 4, "Comment faire du stop partout ?", "Partons en voyage ensemble, je vais vous apprendre à faire du stop efficacement."],
+            [8, 4, "Tutoriel camping sauvage", "Partons en voyage ensemble, je vais vous apprendre comment il est possible de dormir n'importe où uniquement grâce à une tente."],
+            [5, 5, "LE JARDIN DE ROBERT - Boutures", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire des boutures."],
+            [5, 5, "LE JARDIN DE ROBERT - Miel", "Je m'appelle Robert et aujourd'hui nous allons ensemble favoriser l'apparition d'abeilles pour faire du miel."],
+            [5, 5, "LE JARDIN DE ROBERT - Oiseaux", "Je m'appelle Robert et aujourd'hui nous allons ensemble chasser les oiseaux."],
+            [5, 5, "LE JARDIN DE ROBERT - Mildiou", "Je m'appelle Robert et aujourd'hui nous allons ensemble combattre le Mildiou."],
+            [5, 5, "LE JARDIN DE ROBERT - Pesticides", "Je m'appelle Robert et aujourd'hui nous allons ensemble utiliser des pesticides."],
+            [5, 5, "LE JARDIN DE ROBERT - Bouillie Bordelaise", "Je m'appelle Robert et aujourd'hui nous allons ensemble apprendre à faire de la bouillie bordelaise."],
+            [5, 5, "LE JARDIN DE ROBERT - Orangers", "Je m'appelle Robert et aujourd'hui nous allons ensemble entretenir vos orangers."],
+            [5, 5, "LE JARDIN DE ROBERT - Haricots", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire pousser des haricots."],
+            [5, 5, "LE JARDIN DE ROBERT - Permaculture", "Je m'appelle Robert et aujourd'hui nous allons ensemble apprendre à faire de la permaculture."],
+            [5, 5, "LE JARDIN DE ROBERT - Engrais", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire de l'engrais naturel."],
+            [5, 5, "LE JARDIN DE ROBERT - Eau de pluie", "Je m'appelle Robert et aujourd'hui nous allons ensemble apprendre à récupérer l'eau de pluie pour arroser ses plantes."],
+            [5, 5, "LE JARDIN DE ROBERT - Tailler le bambou", "Je m'appelle Robert et aujourd'hui nous allons ensemble à tailler votre forêt de bambou."],
+            [5, 5, "LE JARDIN DE ROBERT - Gestion d'une serre", "Je m'appelle Robert et aujourd'hui nous allons ensemble apprendre à gérer une serre et la température à l'intérieur."],
+            [5, 5, "LE JARDIN DE ROBERT - Couper un arbre", "Je m'appelle Robert et aujourd'hui nous allons ensemble couper un arbre. Oui. Chez vous. Un bel arbre de préférence."],
+            [5, 5, "LE JARDIN DE ROBERT - Faire un verger", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire un verger sur votre terrain."],
+            [5, 5, "LE JARDIN DE ROBERT - Entretenir ses roses", "Je m'appelle Robert et aujourd'hui nous allons ensemble entretenir vos rosiers."],
+            [5, 5, "LE JARDIN DE ROBERT - Mûres grimpantes", "Je m'appelle Robert et aujourd'hui nous allons ensemble favoriser l'apparition de mûres grimpants sur vos murs."],
+            [5, 5, "LE JARDIN DE ROBERT - Vigne", "Je m'appelle Robert et aujourd'hui nous allons ensemble planter de la vigne."],
+            [5, 5, "LE JARDIN DE ROBERT - Vin de pêches", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire du vin de pêche à partir de feuilles de pêche de votre jardin."],
+            [5, 5, "LE JARDIN DE ROBERT - Sirop de sureau", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire du sirop de sureau naturel à partir de votre Sureau."],
+            [5, 5, "LE JARDIN DE ROBERT - Mauvaises herbes", "Je m'appelle Robert et aujourd'hui nous allons ensemble combattre les mauvaises herbes qui se trouvent sur votre propriété."],
+            [5, 5, "LE JARDIN DE ROBERT - Chasser les renards", "Je m'appelle Robert et aujourd'hui nous allons ensemble chasser du renard pour éviter qu'il ne mange vos poules."],
+            [5, 5, "LE JARDIN DE ROBERT - Avoir des poules", "Je m'appelle Robert et aujourd'hui nous allons ensemble construire un poulailler et y mettre des poules."],
+            [5, 5, "LE JARDIN DE ROBERT - Avoir une marre", "Je m'appelle Robert et aujourd'hui nous allons ensemble créer une marre."],
+            [5, 5, "LE JARDIN DE ROBERT - Irriguer l'eau", "Je m'appelle Robert et aujourd'hui nous allons ensemble irriguer de l'eau jusqu'à vos plantations."],
+            [5, 5, "LE JARDIN DE ROBERT - Arroser ses semis", "Je m'appelle Robert et aujourd'hui nous allons ensemble arroser vos semis."],
+            [5, 5, "LE JARDIN DE ROBERT - Maïs", "Je m'appelle Robert et aujourd'hui nous allons ensemble cultiver du maïs."],
+            [5, 5, "LE JARDIN DE ROBERT - Arrosoir", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire un comparatif des arrosoirs du marché."],
+            [5, 5, "LE JARDIN DE ROBERT - Bêche", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire un comparatif des bêches de M. Bricolage."],
+            [5, 5, "LE JARDIN DE ROBERT - Faire un trou", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire un trou dans votre jardin."],
+            [5, 5, "LE JARDIN DE ROBERT - Végétaliser une tonnelle", "Je m'appelle Robert et aujourd'hui nous allons ensemble végétaliser votre tonnelle."],
+            [5, 5, "LE JARDIN DE ROBERT - Radis", "Je m'appelle Robert et aujourd'hui nous allons ensemble faire pousser des radis."]
         ];
 
         foreach($serviceDataSets as $insertedDataSet) {
-            $resInsert = Service::addService($serviceStartDate, $serviceEndDate, $insertedDataSet[0], random_int(90, 160), $insertedDataSet[1], $insertedDataSet[2]);
+            $resInsert = Service::addService($serviceStartDate, $serviceEndDate, $insertedDataSet[0], random_int(90, 160), $insertedDataSet[1], $insertedDataSet[2], $insertedDataSet[3]);
             
             if(!$resInsert) {
                 print ("ERROR WITH SERVICE SEEDER INSERT WITH VALUES : " . $insertedDataSet[0] . $insertedDataSet[1] . $insertedDataSet[2]);
@@ -84,74 +117,6 @@ class controllerSeeder {
 
         print "L'insertion des données de test s'est bien passée. Vous pouvez maintenant vous connecter à un compte utilisateur de test.";
     }
-
-
-
-    /*  USER ADD MODEL
-
-        public static function addUser($name, $password, $email, $phone_number, $balance)
-        {
-            $query = "INSERT INTO users (name, password, email, phone_number, balance) VALUES (:name, :password, :email, :phone_number, :balance)";
-            $p_query = Connexion::pdo()->prepare($query);
-            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            // Need to add a hash for the password
-            $values = array(
-                "name" => $name,
-                "password" => $hashed_password,
-                "email" => $email,
-                "phone_number" => $phone_number,
-                "balance" => $balance
-            );
-            try {
-                $p_query->execute($values);
-                return true;
-            } catch (PDOException $e) {
-                echo "erreur : " . $e->getMessage() . "<br>";
-                return false;
-            }
-        }
-
-    */
-
-    /*  TYPE SERVICE ADD MODEL
-        public static function addTypeService($name) {
-            $query = "INSERT INTO types_services (name) VALUES (:name)";
-            $p_query = Connexion::pdo()->prepare($query);
-		    $values = array(
-		    	"name" => $name,
-		    );
-		    try {
-		    	$p_query->execute($values);
-		    	return true;
-		    } catch (PDOException $e) {
-		    	echo "erreur : ".$e->getMessage()."<br>";
-		    	return false;
-		    }
-        }
-    */
-
-    /*  SERVICE ADD MODEL
-        public static function addService($date_start, $date_end, $id_type_service, $price, $id_user, $title) {
-        $query = "INSERT INTO services (date_start, date_end, id_type_service, price, id_user, title) VALUES (:date_start, :date_end, :id_type_service, :price, :id_user, :title)";
-        $p_query = Connexion::pdo()->prepare($query);
-		$values = array(
-			"date_start" => $date_start,
-			"date_end" => $date_end,
-			"id_type_service" => $id_type_service,
-            "price" => $price,
-            "id_user" => $id_user,
-            "title" => $title
-		);
-		try {
-			$p_query->execute($values);
-			return true;
-		} catch (PDOException $e) {
-			echo "erreur : ".$e->getMessage()."<br>";
-			return false;
-		}
-    }
-    */
-
 }
 
 ?>
--- db_creation.sql
+++ db_creation.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4deb2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : ven. 10 juin 2022 à 11:55
--- Version du serveur :  10.5.15-MariaDB-0+deb11u1
--- Version de PHP : 7.4.28
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 09 nov. 2021 à 18:23
+-- Version du serveur :  8.0.21
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,64 +18,24 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `ajust`
+-- Base de données : `cinquaine`
 --
+
+CREATE DATABASE IF NOT EXISTS cinquaine;
+
+USE cinquaine;
 
 -- --------------------------------------------------------
-
---
--- Structure de la table `services`
---
-
-CREATE TABLE `services` (
-  `id_service` int(11) NOT NULL,
-  `date_start` datetime NOT NULL,
-  `date_end` datetime NOT NULL,
-  `id_type_service` int(11) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `title` varchar(55) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `description` varchar(333) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `services`
---
-
-INSERT INTO `services` (`id_service`, `date_start`, `date_end`, `id_type_service`, `price`, `id_user`, `title`, `image`, `description`) VALUES
-(44, '2021-12-29 19:43:00', '2021-12-29 19:43:01', 1, '1.00', 1, 'Title', '0', 'description vide');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `service_acheteurs`
---
-
-CREATE TABLE `service_acheteurs` (
-  `id_user` int(11) NOT NULL,
-  `id_service` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
 --
 -- Structure de la table `types_services`
 --
 
-CREATE TABLE `types_services` (
-  `id_type_service` int(11) NOT NULL,
-  `name` varchar(55) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `types_services`
---
-
-INSERT INTO `types_services` (`id_type_service`, `name`) VALUES
-(1, 'a'),
-(3, 'test'),
-(4, 'test');
+DROP TABLE IF EXISTS `types_services`;
+CREATE TABLE IF NOT EXISTS `types_services` (
+  `id_type_service` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(55) NOT NULL,
+  PRIMARY KEY (`id_type_service`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -83,93 +43,79 @@ INSERT INTO `types_services` (`id_type_service`, `name`) VALUES
 -- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
-  `id_user` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
   `name` varchar(55) NOT NULL,
   `password` varchar(255) NOT NULL,
   `balance` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `phone_number` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `phone_number` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Déchargement des données de la table `users`
+-- Structure de la table `services`
 --
 
-INSERT INTO `users` (`id_user`, `name`, `password`, `balance`, `email`, `phone_number`) VALUES
-(1, 'pablo', 'tes', 10, 'a', '01981901'),
-(2, 'pablito', 'escobart', 0, '', ''),
-(3, 'test', 'a', 20, 'b', '2'),
-(4, 'test', 'a', 20, 'b', '2'),
-(9, 'b', 'a', 20, 'b', '2'),
-(11, 'test', 'a', 20, 'b', '2'),
-(12, 'user', '$2y$10$KVhl.NjNLVN2.rUls8IbH.AncJXeQb5zF6NgcOJZfjgJRxJk.0X1K', 0, 'user@test.com', '0123456789'),
-(14, 'Flo', '$2y$10$JfyuzkTPesxI.kqE/Iok8ez7W/gN4hWshnLDc4/DjwHO8Jv5.is7.', 0, 'user@test.flo', '1679843058');
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `services`
---
-ALTER TABLE `services`
-  ADD PRIMARY KEY (`id_service`),
-  ADD KEY `id_type_service_fk` (`id_type_service`),
-  ADD KEY `id_user_fk` (`id_user`);
-
---
--- Index pour la table `service_acheteurs`
---
-ALTER TABLE `service_acheteurs`
-  ADD KEY `id_service` (`id_service`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Index pour la table `types_services`
---
-ALTER TABLE `types_services`
-  ADD PRIMARY KEY (`id_type_service`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `services`
---
-ALTER TABLE `services`
-  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-
---
--- AUTO_INCREMENT pour la table `types_services`
---
-ALTER TABLE `types_services`
-  MODIFY `id_type_service` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+DROP TABLE IF EXISTS `services`;
+CREATE TABLE IF NOT EXISTS `services` (
+  `id_service` int NOT NULL AUTO_INCREMENT,
+  `date_start` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `id_type_service` int NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `id_user` int NOT NULL,
+  `title` varchar(55) NOT NULL,
+  `description` varchar(333) NOT NULL,
+  PRIMARY KEY (`id_service`),
+  KEY `id_type_service_fk` (`id_type_service`),
+  KEY `id_user_fk` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
+-- Contraintes pour la table `services`
+--
+ALTER TABLE `services`
+  ADD CONSTRAINT `id_type_service_fk` FOREIGN KEY (`id_type_service`) REFERENCES `types_services` (`id_type_service`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `id_user_fk` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `service_acheteurs`
+--
+
+
+CREATE TABLE `service_acheteurs` (
+  `id_user` int(11) NOT NULL,
+  `id_service` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
 -- Contraintes pour la table `service_acheteurs`
 --
+
 ALTER TABLE `service_acheteurs`
-  ADD CONSTRAINT `service_acheteurs_ibfk_1` FOREIGN KEY (`id_service`) REFERENCES `services` (`id_service`),
-  ADD CONSTRAINT `service_acheteurs_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+ADD KEY `id_service` (`id_service`),
+ADD KEY `id_user` (`id_user`);
+
+ALTER TABLE `service_acheteurs`
+ADD CONSTRAINT `service_acheteurs_ibfk_1` FOREIGN KEY (`id_service`) REFERENCES `services` (`id_service`),
+ADD CONSTRAINT `service_acheteurs_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
 COMMIT;
+
+
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
