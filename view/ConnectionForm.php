98f5f4d33ced7c373694593e91a8c8862b51b42f
Perez Pablo <pablo.perez1@universite-paris-saclay.fr>
2022-06-10 14:56:27 +0200

    update ConnectionForm and RegisterForm

--- view/ConnectionForm.php
+++ view/ConnectionForm.php
@@ -1,21 +1,59 @@
-<div class="container">
-    <form action="index.php?action=login&controller=controllerLogin" method="post" class="loginform">
+<section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-        <div class="form-group">
-            <label for="uname" ><b>Nom d'utilisateur:</b></label>
-            <input type="text" placeholder="Entrez votre nom d'utilisateur" name="uname" required>
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Connexion</p>
+
+                <form class="mx-1 mx-md-4" action="index.php?action=login&controller=controllerLogin" method="post" class="loginform">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <label class="form-label" for="uname">Votre nom</label>
+                      <input type="text" name="uname" class="form-control" placeholder="Entrer votre nom" />
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <label class="form-label" for="psw">Votre mot de passe</label>
+                      <input type="password" name="psw" class="form-control" placeholder="Entrer votre mot de passe" />
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button class="btn btn-primary btn-lg"  type="submit" class="btn btn-primary btn-lg">Connexion</button>
+                  </div>
+
+                  <div class="form-check d-flex justify-content-center mb-1">
+                    <input class="form-check-input me-2" type="checkbox" name="remember" checked="checked" />
+                    <label class="form-check-label">
+                      Se souvenir du mot de passe
+                    </label>
+                  </div>
+                  <div class="form-check d-flex justify-content-center mb-4">
+                    <div class="psw" style="margin-top: 10px">
+                    <a href="#">Mot de passe oublié?</a>
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button class="btn btn-primary"><a class="btn btn-primary" href="index.php?controller=controllerLogin&action=displayRegister">Inscription</a></button>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-            <label for="psw"><b>Mot de passe:</b></label>
-            <input type="password" placeholder="Entrer le mot de passe" name="psw" required>
-        </div>
-        <button class="btn" type="submit">Connexion</button>
-        <div><button class="btn btnregister"><a class="btn" href="index.php?controller=controllerLogin&action=displayRegister">Inscription</a></button></div>
-        <div class="form-check" style="margin-top: 10px; margin-right: 20px; ">
-            <input type="checkbox" checked="checked" name="remember">
-            <label>Se souvenir de moi</label>
-        </div>
-        <div class="psw" style="margin-top: 10px"><a href="#">Mot de passe oublié?</a></div>
-        
-    </form>
-</div>
+      </div>
+    </div>
+  </div>
+</section>
--- view/RegisterForm.php
+++ view/RegisterForm.php
@@ -1,29 +1,59 @@
+<section class="vh-100" style="background-color: #eee;">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" style="border-radius: 25px;">
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-<div class="container">
-    <form action="index.php?action=register&controller=controllerLogin" method="post" class="loginform  ">
-        <div class="form-group">
-            <label for="uname"><b>Login :</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Inscription</p>
+
+                <form class="mx-1 mx-md-4" action="index.php?action=register&controller=controllerLogin" method="post" class="loginform">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <label class="form-label" for="uname">Votre nom</label>
+                      <input type="text" name="uname" class="form-control" placeholder="Entrer votre nom" />
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <label class="form-label" for="email">Votre adresse mail</label>
+                      <input type="email" name="email" class="form-control" placeholder="Entrer votre adresse mail" />
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <label class="form-label" for="psw">Votre mot de passe</label>
+                      <input type="password" name="psw" class="form-control" placeholder="Entrer votre mot de passe" />
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <label class="form-label" for="phone_number">Votre numéro de téléphone</label>
+                      <input type="tel" name="phone_number" class="form-control" pattern="[0-9]{10}" required placeholder="Entrer votre numéro de téléphone" />
+                    </div>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button class="btn btn-primary"  type="submit" class="btn btn-primary btn-lg">S'inscrire</button>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div class="form-group">
-            <label for="psw"><b>Mot de passe :</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-        </div>
-
-        <div class="form-group">
-            <label for="email" style="margin-left: 8vh;"><b>Email :</b></label>
-            <input type="email" placeholder="Enter your mail" name="email" required>
-        </div>
-
-        <div class="form-group">
-            <label for="phone_number"><b>Téléphone :</b></label>
-            <input type="tel" placeholder="0768656887" name="phone_number" pattern="[0-9]{10}" required>
-        </div>
-
-        <button class="btn" type="submit">Inscription</button>
+      </div>
     </div>
-    <div class="container" style="background-color:#f1f1f1">
-        <span class="psw">Retour <a href="#">retour</a></span>
-    </form>
-</div>
+  </div>
+</section>
