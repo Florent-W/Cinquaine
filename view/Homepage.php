0e04d7120cc70ddb06e660ba53692f0506310160
toemah <toemah@protonmail.com>
2022-06-10 18:03:35 +0200

    Update Homepage.php

--- view/Homepage.php
+++ view/Homepage.php
@@ -1,6 +1,6 @@
 <!-- Section-->
-<div class="container px-4 px-lg-5 mt-5">
-    <div class="row g-2 row-cols-2 row-cols-lg-3 row-cols-xl-4 gap-2 mb-2 justify-content-center">
+<div class="container-fluid">
+    <div class="row row-cols-2 row-cols-lg-3 row-cols-xl-4 gap-2 mb-2 justify-content-center">
         <?php
         foreach ($services as $service) {
             echo "<div class='col' style='height: 600px; width: 500px'>";
