27f8c259f0f19c4bcdab1e2e1b83dbf1485a9262
toemah <toemah@protonmail.com>
2022-06-10 17:21:44 +0200

    Update Homepage.php

--- view/Homepage.php
+++ view/Homepage.php
@@ -1,39 +1,11 @@
 <!-- Section-->
 <div class="container px-4 px-lg-5 mt-5">
-    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 gap-2 mb-2 justify-content-center">
         <?php
             foreach($services as $service) {
-              echo "
-                <div class='col mb-5'>
-                    <div class='card h-100'>
-                        <div class='espaceformodal' data-bs-toggle='modal' data-bs-target='#DescriptionModal'>
-                        <!-- Product image-->
-                        <img class='card-img-top' src='assets/image/dessin.jpg' alt='...' />
-                        <!-- Product details-->
-                        <div class='card-body p-4'>
-                            <div class='text-center'>
-                                <!-- Product name-->
-                                <h5 class='fw-bolder'>{$service->getTitle()}</h5>
-                                <!-- Product reviews-->
-                                <div class='d-flex justify-content-center small text-warning mb-2'>
-                                    <div class='bi-star-fill'></div>
-                                    <div class='bi-star-fill'></div>
-                                    <div class='bi-star-fill'></div>
-                                    <div class='bi-star-fill'></div>
-                                    <div class='bi-star-fill'></div>
-                                </div>
-                                <!-- Product price-->
-                                <h5 class='fw-bolder'>€{$service->getPrice()}</h5>
-                            </div>
-                        </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class='card-footer p-4 pt-0 border-top-0 bg-transparent'>
-                            <div class='text-center'><a class='btn btn-outline-dark mt-auto' href='#'>Ajouter au panier</a></div>
-                        </div>
-                    </div>
-                </div>
-                ";
+                echo "<div style='height: 600px; width: 500px'>";
+                require "ServiceCard.php";
+                echo "</div>";
             }
         ?>
     </div>
