26d20aa3fa3dcb4bf5a83548b8631a5e6c25253c
toemah <toemah@protonmail.com>
2022-06-10 17:30:45 +0200

    Update ServiceCard.php

--- view/ServiceCard.php
+++ view/ServiceCard.php
@@ -33,10 +33,10 @@ $imgSrc = "assets/image/" . $serviceName . ".jpg";
         </div>
     </div>
     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-        <?php if($_SESSION["id"] == $service->getIdUser()): ?>
+        <?php if(isset($_SESSION["id"]) && $_SESSION["id"] == $service->getIdUser()): ?>
         <div class="text-center"><a style="border: none!important;" class="btn btn-outline-danger mt-auto" href="./index.html?controller=controllerHome&action=deleteService">Supprimer</a></div>
         <?php else: ?>
-        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Acheter</a></div>
+        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="./index.php?controller=controllerLogin&action=displayLogin">Acheter</a></div>
         <?php endif ?>
     </div>
 </div>
