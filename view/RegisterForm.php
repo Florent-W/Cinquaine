7c3da3824fb71c0fda7e5b99708f8f0c5e531880
Perez Pablo <pablo.perez1@universite-paris-saclay.fr>
2022-06-10 16:04:35 +0200

    update style ConnectionForm RegisterForm

--- view/RegisterForm.php
+++ view/RegisterForm.php
@@ -2,7 +2,7 @@
   <div class="container h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style="border-radius: 25px;">
+        <div class="card text-black" style="border-radius: 25px; background: rgba(31, 101, 126, 0.4);">
           <div class="card-body p-md-5">
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
@@ -44,7 +44,7 @@
                   </div>
 
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button class="btn btn-primary btn-lg"  type="submit" class="btn btn-primary btn-lg">S'inscrire</button>
+                    <button class="btn btn-lg"  type="submit">S'inscrire</button>
                   </div>
 
                 </form>
