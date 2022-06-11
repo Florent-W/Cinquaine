26ae75acca9b9bd8c77edbd32ef53fb580460e47
toemah <toemah@protonmail.com>
2022-06-11 18:39:33 +0200

    Update RegisterForm.php
    formatting

--- view/RegisterForm.php
+++ view/RegisterForm.php
@@ -6,11 +6,8 @@
           <div class="card-body p-md-5">
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
                 <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Inscription</p>
-
                 <form class="mx-1 mx-md-4" action="index.php?action=register&controller=controllerLogin" method="post" class="loginform">
-
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
@@ -18,7 +15,6 @@
                       <input type="text" name="uname" class="form-control" placeholder="Entrer votre nom" />
                     </div>
                   </div>
-
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
@@ -26,7 +22,6 @@
                       <input type="email" name="email" class="form-control" placeholder="Entrer votre adresse mail" />
                     </div>
                   </div>
-
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
@@ -34,7 +29,6 @@
                       <input type="password" name="psw" class="form-control" placeholder="Entrer votre mot de passe" />
                     </div>
                   </div>
-
                   <div class="d-flex flex-row align-items-center mb-5">
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
@@ -42,13 +36,10 @@
                       <input type="tel" name="phone_number" class="form-control" pattern="[0-9]{10}" required placeholder="Entrer votre numéro de téléphone" />
                     </div>
                   </div>
-
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button class="btn btn-lg"  type="submit">S'inscrire</button>
+                    <button class="btn btn-lg" type="submit">S'inscrire</button>
                   </div>
-
                 </form>
-
               </div>
             </div>
           </div>
