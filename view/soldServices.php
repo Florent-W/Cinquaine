d6e37b58aa678aad0cff9574ae513f5b27f0ade2
toemah <toemah@protonmail.com>
2022-06-13 18:21:47 +0200

    Create soldServices.php

---
+++ view/soldServices.php
@@ -0,0 +1,21 @@
+<div class="container pt-2 pb-2">
+    <div class="row justify-content-center text-dark formprofil">
+        <div class="col-lg-7 order-md-first">
+            <div class="d-grid gap-2">
+                <?php
+                foreach ($services as $service) {
+                    echo "<div class='row justify-content-center text-dark formprofil'>";
+                    echo "<div class='col-md'>";
+                    require("ServiceCard.php");
+                    echo "<div class='d-flex'>";
+                    echo "<b>" . User::getUserById($service->getIdUser())->getName() . ":</b>";
+                    echo "<p>Je suis le message que j'ai écrit en commandant</p>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "</div>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
