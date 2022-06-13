febbcb30d724264367e5de1aac6a98af950c102b
Jacques-Portable <jacques.clery--salenave@u-psud.fr>
2022-06-13 14:27:32 +0200

    bought services ready for bought message implementation

--- view/boughtServices.php
+++ view/boughtServices.php
@@ -1,11 +1,20 @@
 
 <div class="container pt-2 pb-2">
     <div class="row justify-content-center text-dark formprofil">
-        <div class="col-md-6 order-md-first">
+        <div class="col-lg-7 order-md-first">
             <div class="d-grid gap-2">
                 <?php
                     foreach ($services as $service) {
+                        echo
+                        "<div class='row justify-content-center text-dark formprofil'>
+                                <div class='col-md'>";
                         require("ServiceCard.php");
+
+                        echo "
+
+                            <p>Je suis le message que j'ai écrit en commandant</p>
+                            </div>
+                        </div>";
                     }
                 ?>
             </div>
