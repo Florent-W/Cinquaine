b933f0a286ec840928970af81c427d85098a33b0
toemah <toemah@protonmail.com>
2022-06-14 17:02:47 +0200

    soldServices navitem active state

--- view/soldServices.php
+++ view/soldServices.php
@@ -1,3 +1,8 @@
+<style>
+    .nav-item:nth-child(5){
+        background: rgba(31, 101, 126, 0.6);
+    }
+</style>
 <div class="container pt-2 pb-2">
     <div class="row justify-content-center text-dark formprofil">
         <div class="col-lg-7 order-md-first">
